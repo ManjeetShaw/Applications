6f30e9572df2509aaea34ddb545aec4f9f927821
Manjeet Shaw <manjeetshaw07@gmail.com>
2026-01-15 00:31:59 +0530

    INITIAL COMMIT

---
+++ routes/user.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/signup', handleUserSignup);
+
+router.post('/login', handleUserLogin);
+module.export = router;
